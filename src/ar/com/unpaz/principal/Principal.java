e5f106f0e491224a5f1da63ab0a0444f77935f39
rubendelviso <rubendelviso13@gmail.com>
2026-03-11 01:38:04 -0300

    cambios

--- src/ar/com/unpaz/principal/Principal.java
+++ src/ar/com/unpaz/principal/Principal.java
@@ -15,20 +15,24 @@ public class Principal {
 		System.out.println("El titular de la cuenta es:" + cuenta1.getTitular());
 		cuenta1.setSaldo(0);
 		System.out.println("El saldo de la cuenta es" + cuenta1.getSaldo());
+		
+		
 		// TODO Austo-generated method stub
 		
 		Contacto contacto1 = new Contacto();
 		contacto1.setCodigoPostal(1563);
+		System.out.println("Hacer sets y gets de class contacto");
 		
-
-		
-		Cliente cliente1 = new Cliente();
-		cliente1.setNumeroCliente(1000);
-		System.out.println(cliente1);
-		cliente1.setCuenta(cuenta1);
-		System.out.println(cuenta1);
-		cliente1.setContacto(contacto1);
-		System.out.println(contacto1);
+//		
+//
+//		
+//		Cliente cliente1 = new Cliente();
+//		cliente1.setNumeroCliente(1000);
+//		System.out.println(cliente1);
+//		cliente1.setCuenta(cuenta1);
+//		System.out.println(cuenta1);
+//		cliente1.setContacto(contacto1);
+//		System.out.println(contacto1);
 	}
 
 }
